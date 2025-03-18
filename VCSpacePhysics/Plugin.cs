482739acb3c8f4162306318285c0d74ab13389d2
Yoshi Walsh <yoshi@yoshiwalsh.me>
2025-03-19 01:27:09 +1100

    Mod skeleton

---
+++ VCSpacePhysics/Plugin.cs
@@ -0,0 +1,38 @@
+﻿using BepInEx;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using Gameplay.SpacePlatforms;
+using Gameplay.Ship;
+using Photon.Pun;
+using CG.Input;
+using UnityEngine.Localization;
+
+namespace VCSpacePhysics
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            
+            // Configure Harmony
+            var harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+            var assembly = Assembly.GetExecutingAssembly();
+            harmony.PatchAll();
+
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+
+        private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+
+        }
+    }
+}
