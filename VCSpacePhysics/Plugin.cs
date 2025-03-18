b59d1c414a26b55a453db5077f4ac4b82c4134d4
Yoshi Walsh <yoshi@yoshiwalsh.me>
2025-03-19 01:28:22 +1100

    Alter keybind names

--- VCSpacePhysics/Plugin.cs
+++ VCSpacePhysics/Plugin.cs
@@ -45,5 +45,46 @@ namespace VCSpacePhysics
         {
             return false;
         }
+
+        [HarmonyPrefix, HarmonyPatch(typeof(InputKeyBinding), nameof(InputKeyBinding.RebuildKeybindArray), [])]
+        public static void RebuildKeybindArray(InputKeyBinding __instance)
+        {
+            var ControlRenames = new Dictionary<string, Dictionary<string, DefaultableLocalizedString>>
+            {
+                ["Ship"] = new Dictionary<string, DefaultableLocalizedString>
+                {
+                    ["Move Forward"] = new DefaultableLocalizedString("Thrust Forward", new LocalizedString()),
+                    ["Move Backward"] = new DefaultableLocalizedString("Thrust Backward", new LocalizedString()),
+                    ["Rotate Left"] = new DefaultableLocalizedString("Thrust Left", new LocalizedString()),
+                    ["Rotate Right"] = new DefaultableLocalizedString("Thrust Right", new LocalizedString()),
+                    ["Move Up"] = new DefaultableLocalizedString("Thrust Up", new LocalizedString()),
+                    ["Move Down"] = new DefaultableLocalizedString("Thrust Down", new LocalizedString()),
+                    ["Move Left"] = new DefaultableLocalizedString("Roll Counter-clockwise", new LocalizedString()),
+                    ["Move Right"] = new DefaultableLocalizedString("Roll Clockwise", new LocalizedString()),
+                    ["Thruster Boost"] = new DefaultableLocalizedString("Dash / Boost", new LocalizedString()),
+                }
+            };
+
+            foreach (SettingsControlList.Group group in __instance.KeyBindDisplayList().groups)
+            {
+                if (group.inputActionMapName == "Ship")
+                {
+                    group.Name = new DefaultableLocalizedString("Additional Controls (Ship / EVA Jetpack)", new LocalizedString());
+                }
+                if (group.Name.FallBackString == "Additional Controls (EVA Jetpack)")
+                {
+                    __instance.KeyBindDisplayList().groups.Remove(group);
+                }
+
+                if (ControlRenames.ContainsKey(group.inputActionMapName))
+                {
+                    var groupEntriesMap = ControlRenames.GetValueOrDefault(group.inputActionMapName);
+                    foreach (SettingsControlList.InputEntry entry in group.InputEntrys)
+                    {
+                        entry.Name = groupEntriesMap.GetValueOrDefault(entry.Name.FallBackString, entry.Name);
+                    }
+                }
+            }
+        }
     }
 }
