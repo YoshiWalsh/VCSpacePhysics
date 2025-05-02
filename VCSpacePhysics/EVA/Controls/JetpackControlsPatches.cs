2bf70b9c9c324bc3433206691d63f2aad4c8b69d
Yoshi Walsh <yoshi@yoshiwalsh.me>
2025-05-02 18:14:36 +1000

    Pitch player around center-of-gravity instead of feet
    This introduces a new bug: now the player's momentum is not applied on frames when their pitch changed. Not sure why this is happening yet.

--- VCSpacePhysics/EVA/Controls/JetpackControlsPatches.cs
+++ VCSpacePhysics/EVA/Controls/JetpackControlsPatches.cs
@@ -12,14 +12,24 @@ namespace VCSpacePhysics.EVA.Controls
     [HarmonyPatch]
     public class JetpackControlsPatches
     {
+        private static Vector3 CenterOfGravityOffset = new Vector3(0, 1.2f, 0f);
+
+        private static void RotatePlayerPositionAroundCenterOfGravity(CharacterLocomotion character, Quaternion rotation)
+        {
+            Vector3 characterCOGWorldspace = character.gameObject.transform.TransformPoint(CenterOfGravityOffset);
+            Vector3 currentRootPositionRelativeToCOG = character.gameObject.transform.position - characterCOGWorldspace;
+            Vector3 newRootPositionRelativeToCOG = rotation * currentRootPositionRelativeToCOG;
+            character.gameObject.transform.position = newRootPositionRelativeToCOG + characterCOGWorldspace;
+        }
+
         [HarmonyPostfix, HarmonyPatch(typeof(FirstPerson), nameof(FirstPerson.Rotate))]
         static void FirstPersonRotate(FirstPerson __instance, float horizontalMovement, float verticalMovement, bool immediateUpdate)
         {
             if (EVAUtils.IsPlayerFlying(__instance.m_CharacterLocomotion))
             {
                 // Make looking up/down rotate the entire character, not just the head
-                // TODO: Tweak maths to rotate player around center-of-gravity instead of feet.
                 var rotation = Quaternion.AngleAxis(__instance.m_Pitch, __instance.m_CharacterLocomotion.transform.right);
+                RotatePlayerPositionAroundCenterOfGravity(__instance.m_CharacterLocomotion, rotation);
                 __instance.m_BaseRotation = rotation * __instance.m_BaseRotation;
                 __instance.m_Pitch = 0f;
 
