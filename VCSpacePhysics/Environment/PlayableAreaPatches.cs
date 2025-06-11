efe0b83b4034cca32675a2bad034e768745db5e3
Yoshi Walsh <yoshi@yoshiwalsh.me>
2025-06-12 00:47:39 +1000

    Disable play area size limit

---
+++ VCSpacePhysics/Environment/PlayableAreaPatches.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VCSpacePhysics.Environment
+{
+    [HarmonyPatch]
+    public class PlayableAreaPatches
+    {
+        [HarmonyPrefix, HarmonyPatch(typeof(PlayableAreaController), nameof(PlayableAreaController.FixedUpdate))]
+        static bool PlayableAreaControllerFixedUpdate()
+        {
+            return false;
+        }
+    }
+}
